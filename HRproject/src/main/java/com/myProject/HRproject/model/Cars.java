ab5a2aa5cd17af4e709732d9a60de45650af9aaa
VolodymyrZee <emkilvolodymyr@gmail.com>
2020-09-08 13:26:28 -0500

    updates/cleanup

--- HRproject/src/main/java/com/myProject/HRproject/model/Cars.java
+++ HRproject/src/main/java/com/myProject/HRproject/model/Cars.java
@@ -1,6 +1,6 @@
 package com.myProject.HRproject.model;
 
-import javax.persistence.CascadeType;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,7 +8,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
